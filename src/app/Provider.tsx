82b2b9da7fcc675612cedfa62b4d3888a403ea7f
DecidedCard <heajun35@gmail.com>
2024-03-29 20:10:39 +0900

    Feat: react-query provider setting

---
+++ src/app/Provider.tsx
@@ -0,0 +1,9 @@
+import React, { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const Provider = ({ children }: PropsWithChildren) => {
+    const queryClient = new QueryClient();
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
+
+export default Provider;
