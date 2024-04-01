4082daeaeec733d3621d1bad06c283e8f1c0ae50
DecidedCard <heajun35@gmail.com>
2024-04-01 22:02:49 +0900

    Feat: 소개 인풋 작성

--- src/Components/MyPageInput/Introduction.tsx
+++ src/Components/MyPageInput/Introduction.tsx
@@ -1,11 +1,14 @@
-import useInput from "@/hooks/useInput";
+"use client";
+
+import useBasicInfo from "@/hooks/mypage/useBasicInfo";
 import React from "react";
 
 const Introduction = () => {
+    const { introduce, onChangeIntroduceHandler } = useBasicInfo();
     return (
         <div>
             <label>소개: </label>
-            <input type="text" />
+            <input type="text" value={introduce} onChange={onChangeIntroduceHandler} />
         </div>
     );
 };
