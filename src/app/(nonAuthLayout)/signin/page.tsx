2adf285d42fc21ced5985fa4acabbc8e9cb7dd51
sangho <keumm9635@gmail.com>
2024-04-02 21:17:40 +0900

    Feat: 자체 회원가입 기능 구현

--- src/app/(nonAuthLayout)/signin/page.tsx
+++ src/app/(nonAuthLayout)/signin/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React from "react";
 import { supabase } from "@/util/supabase/clientSupabase";
+import useInput from "@/hooks/useInput";
 
 const SignIn = () => {
+    const [email, onChangeEmailHandler] = useInput();
+    const [password, onChangePasswordHandler] = useInput();
+
     const signInWithGithub = async () => {
-        ``;
         const { data, error } = await supabase.auth.signInWithOAuth({
             provider: "github",
         });
@@ -19,26 +22,46 @@ const SignIn = () => {
         }
     };
 
-    // const signInWithKakao = async () => {
-    //     const { data, error } = await supabase.auth.signInWithOAuth({
-    //         provider: "kakao",
-    //     });
-    //     console.log(data);
-    //     try {
-    //         if (error) {
-    //             throw new Error("카카오 로그인 오류가 발생하였습니다.");
-    //         }
-    //         console.log(data);
-    //         return;
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
+    const signInWithKakao = async () => {
+        const { data, error } = await supabase.auth.signInWithOAuth({
+            provider: "kakao",
+        });
+        console.log(data);
+        try {
+            if (error) {
+                throw new Error("카카오 로그인 오류가 발생하였습니다.");
+            }
+            console.log(data);
+            return;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const onSubmitLoginUser = async () => {
+        const {
+            data: { user },
+        } = await supabase.auth.getUser();
+
+        console.log(user);
+        alert("gkdl");
+    };
+
     return (
         <div>
-            SignIn
-            <button onClick={signInWithGithub}>깃허브 확인 버튼</button>
-            {/*<button onClick={signInWithKakao}>카카오 확인 버튼</button>*/}
+            <form onSubmit={onSubmitLoginUser}>
+                <div>
+                    이메일:&nbsp;
+                    <input value={email} placeholder="이메일을 작성해주세요" onChange={onChangeEmailHandler} />
+                </div>
+                <div>
+                    비밀번호:&nbsp;
+                    <input value={password} placeholder="비밀번호를 작성해주세요" onChange={onChangePasswordHandler} />
+                </div>
+                <button>로그인</button>
+            </form>
+            <button onClick={signInWithGithub}>깃허브 로그인 버튼&nbsp;</button>
+            <button onClick={signInWithKakao}>카카오 로그인 버튼</button>
         </div>
     );
 };
