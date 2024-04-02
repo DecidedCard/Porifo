705a6e33ba232a76f874bfa5f0370bce287a7d03
CodHan <qpscm662@naver.com>
2024-04-02 19:43:38 +0900

    Feat: 무한스크롤

--- src/Components/Community/Cards.tsx
+++ src/Components/Community/Cards.tsx
@@ -1,19 +1,28 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
 
 import { getTodo } from "./api";
 
 const Cards = () => {
-    const { isLoading, data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    const { isLoading, data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
         queryKey: ["todos"],
         queryFn: getTodo,
-        initialPageParam: 19,
+        initialPageParam: 1,
         getNextPageParam: (lastPage, allPages) => {
             return allPages.length + 1;
         },
     });
+
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (inView && hasNextPage) {
+            fetchNextPage();
+        }
+    }, [inView, hasNextPage]);
 
     if (isLoading) {
         return <div>로딩중 .. !</div>;
@@ -30,13 +39,9 @@ const Cards = () => {
                     );
                 });
             })}
-            {hasNextPage ? (
-                <button className="p-10 text-red-600" onClick={() => fetchNextPage()}>
-                    더 불러오기..
-                </button>
-            ) : (
-                <div>마지막 페이지 입니다.</div>
-            )}
+
+            <div ref={ref}></div>
+            {isFetchingNextPage && <h3>Loding...</h3>}
         </div>
     );
 };
