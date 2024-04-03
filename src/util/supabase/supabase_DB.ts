34709588cc0143af5c153c87264e6cb048315dbc
DecidedCard <heajun35@gmail.com>
2024-04-03 11:18:49 +0900

    Feat: supabase 데이터 가져오는 함수 작성

--- src/util/supabase/supabase_DB.ts
+++ src/util/supabase/supabase_DB.ts
@@ -12,3 +12,26 @@ export const supabaseInsert = async (arg: string, item: any) => {
         Promise.reject(error);
     }
 };
+
+// 특정 값을 가져오거나 아니면 전체 값을 가져올 수 있도록 작성
+export const supabaseRead = async (arg: string, col?: { id: string; value: string }) => {
+    try {
+        if (col) {
+            const { data: portfolioInfo, error } = await supabase.from(arg).select("*").eq(col.id, col.value);
+            if (error) {
+                console.error(error);
+                throw new Error("에러가 발생했습니다.");
+            }
+            return portfolioInfo;
+        } else {
+            const { data: portfolioInfo, error } = await supabase.from(arg).select("*");
+            if (error) {
+                console.error(error);
+                throw new Error("에러가 발생했습니다.");
+            }
+            return portfolioInfo;
+        }
+    } catch (error) {
+        return Promise.reject(error);
+    }
+};
