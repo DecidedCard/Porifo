38d4fd988a5d0384061a3c010e2c92d2f1104f68
CodHan <qpscm662@naver.com>
2024-04-03 19:45:53 +0900

    Refactor: refetch함수 생성

--- src/Components/Community/Cards.tsx
+++ src/Components/Community/Cards.tsx
@@ -1,28 +1,65 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { useInView } from "react-intersection-observer";
+import React, { useEffect, useState } from "react";
 
-import { getTodo } from "./api";
+import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
+import { getPortfolio } from "./api";
 
 const Cards = () => {
+    const [page, setPage] = useState(0);
+    const [filter, setFilter] = useState("기본");
+
+    const queryClient = useQueryClient();
+    const { ref, inView } = useInView();
+
+    const getFromAndTo = () => {
+        const ITEM_PER_PAGE = 5;
+
+        let from = page * ITEM_PER_PAGE;
+        let to = from + ITEM_PER_PAGE;
+
+        if (page > 0) {
+            from += 1;
+        }
+
+        return { from, to };
+    };
+
     const { isLoading, data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
-        queryKey: ["todos"],
-        queryFn: getTodo,
+        queryKey: ["portfolio"],
+        queryFn: () => getPortfolio({ getFromAndTo, filter, page, setPage }),
         initialPageParam: 1,
         getNextPageParam: (lastPage, allPages) => {
+            if (lastPage!.length < 5) {
+                return null;
+            }
             return allPages.length + 1;
         },
+        refetchOnWindowFocus: false,
     });
-
-    const { ref, inView } = useInView();
 
     useEffect(() => {
         if (inView && hasNextPage) {
             fetchNextPage();
         }
-    }, [inView, hasNextPage]);
+        return;
+    }, [inView, hasNextPage, fetchNextPage]);
+
+    const refetch = (filter: string) => {
+        queryClient.removeQueries({ queryKey: ["portfolio"] });
+        setFilter(filter);
+        setPage(0);
+    };
+
+    const handleFilterOption = (filterValue: string) => {
+        if (filterValue === "최신순") {
+            refetch("최신순");
+        }
+        if (filterValue === "기본") {
+            refetch("기본");
+        }
+    };
 
     if (isLoading) {
         return <div>로딩중 .. !</div>;
@@ -30,16 +67,22 @@ const Cards = () => {
 
     return (
         <div>
-            {data?.pages.map((todos) => {
-                return todos.map((item: any) => {
+            <select onChange={(e) => handleFilterOption(e.target.value)}>
+                <option value="정렬" hidden>
+                    {filter}
+                </option>
+                <option value="기본">기본</option>
+                <option value="최신순">최신순</option>
+            </select>
+            {data?.pages.map((portfolio: any) => {
+                return portfolio.map((item: any) => {
                     return (
                         <div key={item.id}>
-                            <div className="border-2 border-solid p-10 flex justify-center">{item.title}</div>
+                            <div className="border-2 border-solid p-20 flex justify-center">{item.name}</div>
                         </div>
                     );
                 });
             })}
-
             <div ref={ref}></div>
             {isFetchingNextPage && <h3>Loding...</h3>}
         </div>
