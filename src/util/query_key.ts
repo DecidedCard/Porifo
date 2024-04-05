19c6170326b78ac781db1931f9ca8473556f8247
CodHan <qpscm662@naver.com>
2024-04-05 23:20:59 +0900

    Feat: detail컴포넌트 생성 및 쿼리 세팅

--- src/util/query_key.ts
+++ src/util/query_key.ts
@@ -2,4 +2,5 @@ export const QUERY_KEY = {
     myPageUser: "myPage/user",
     myPagePortfolio: "myPage/portfolio",
     communityPortfolio: "community/portfolio",
+    detailPortfolio: "detail/portfolio",
 };
