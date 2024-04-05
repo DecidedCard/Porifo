39d6a39f162b64b525ab4b8b645a2395254f1b39
DecidedCard <heajun35@gmail.com>
2024-04-05 16:30:37 +0900

    Refactor: querykey 상수화

--- src/hooks/mypage/usePortfolio.ts
+++ src/hooks/mypage/usePortfolio.ts
@@ -1,14 +1,14 @@
+import { QUERY_KEY } from "@/util/queryKey";
 import { supabasePortfolioInfoRead } from "@/util/supabase/portfolioInfo_supabase_DB";
 import { useQuery } from "@tanstack/react-query";
 
 const usePortfolio = (id: string) => {
-    const queryKey = ["myPage/portfolio"];
     const {
         isError,
         isFetching,
         data: portfolio,
     } = useQuery({
-        queryKey,
+        queryKey: [QUERY_KEY.myPagePortfolio],
         queryFn: () => supabasePortfolioInfoRead({ id: "userId", value: id }),
         retry: 0,
         refetchOnWindowFocus: false,
