5b30970eb8db61e5d4a8f714c2765347d90efae2
DecidedCard <heajun35@gmail.com>
2024-04-06 16:23:16 +0900

    Feat: 프로젝트 유효성 검사 작성

--- src/util/input_form_ validation.ts
+++ src/util/input_form_ validation.ts
@@ -4,37 +4,37 @@ import type { Project } from "@/types/Project";
 // portfolio input form validation
 export const portfolioInputFormValidation = (info: PortfolioInfo) => {
     if (!info.profileImage) {
-        alert("프로필 사진을 넣어주시기 바랍니다.");
+        alert("프로필 사진을 넣어주시기를 바랍니다.");
         return true;
     }
 
     if (!info.name) {
-        alert("이름을 작성해주시기 바랍니다.");
+        alert("이름을 작성해주시기를 바랍니다.");
         return true;
     }
 
     if (!info.englishName) {
-        alert("영문이름을 작성해주시기 바랍니다.");
+        alert("영문이름을 작성해주시기를 바랍니다.");
         return true;
     }
 
     if (!info.birthday) {
-        alert("생년월일을 작성해주시기 바랍니다.");
+        alert("생년월일을 작성해주시기를 바랍니다.");
         return true;
     }
 
     if (!info.tel) {
-        alert("전화버호를 작성해주시기 바랍니다.");
+        alert("전화버호를 작성해주시기를 바랍니다.");
         return true;
     }
 
     if (!info.email) {
-        alert("이메일을 작성해주시기 바랍니다.");
+        alert("이메일을 작성해주시기를 바랍니다.");
         return true;
     }
 
     if (!info.school) {
-        alert("학교를 작성해주시기 바랍니다.");
+        alert("학교를 작성해주시기를 바랍니다.");
         return true;
     }
 
@@ -44,7 +44,7 @@ export const portfolioInputFormValidation = (info: PortfolioInfo) => {
     }
 
     if (!info.project) {
-        alert("project를 작성해주시기 바랍니다.");
+        alert("project를 작성해주시기를 바랍니다.");
         return true;
     }
 
@@ -54,17 +54,17 @@ export const portfolioInputFormValidation = (info: PortfolioInfo) => {
 // project input form validation
 export const projectInputFormValidation = (info: Project) => {
     if (!info.name) {
-        alert("프로젝트 이름을 작성해주시기 바랍니다.");
+        alert("프로젝트 이름을 작성해주시기를 바랍니다.");
         return true;
     }
 
     if (!info.date) {
-        alert("프로젝트 기간을 작성해주시기 바랍니다.");
+        alert("프로젝트 기간을 작성해주시기를 바랍니다.");
         return true;
     }
 
     if (!info.introduce) {
-        alert("프로젝트 소개를 작성해주시기 바랍니다.");
+        alert("프로젝트 소개를 작성해주시기를 바랍니다.");
         return true;
     }
 
@@ -74,7 +74,7 @@ export const projectInputFormValidation = (info: Project) => {
     }
 
     if (!info.deployLink && !info.githubLink) {
-        alert("프로젝트 배포링크 또는 Github링크를 작성해주시기 바랍니다.");
+        alert("프로젝트 배포링크 또는 Github링크를 작성해주시기를 바랍니다.");
         return true;
     }
 
