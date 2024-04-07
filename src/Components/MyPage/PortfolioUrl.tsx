3088304f1213b59f35a5203cc0d694ab94ae89c6
DecidedCard <heajun35@gmail.com>
2024-04-07 19:17:57 +0900

    Style: mypage ui 작성

--- src/Components/MyPage/PortfolioUrl.tsx
+++ src/Components/MyPage/PortfolioUrl.tsx
@@ -2,16 +2,43 @@
 
 import useInfo from "@/hooks/mypage/useInfo";
 import React from "react";
+import Input from "../Commen/Input";
 
 const PortfolioUrl = () => {
     const { basicInfo, onChangeBlogHandler, onChangeGithubHandler } = useInfo();
     return (
-        <div>
-            <label>Blog: </label>
-            <input type="url" value={basicInfo.blogLink!} onChange={onChangeBlogHandler} />
-            <label>Github: </label>
-            <input type="url" value={basicInfo.githubLink!} onChange={onChangeGithubHandler} />
-        </div>
+        <main className="bg-white rounded-2xl mt-10 ml-9 w-[720px] pb-20">
+            <div className="flex flex-col gap-10">
+                <h1 className="flex items-center justify-start pl-6 pt-6 text-2xl font-bold tracking-wider">URL</h1>
+
+                <div className="flex items-start">
+                    <label className="pl-6 font-medium text-zinc-500 relative w-[177px] flex items-center justify-start">
+                        Blog
+                    </label>
+                    <Input
+                        type="url"
+                        width={500}
+                        size="big"
+                        placeholder="http://"
+                        value={basicInfo.blogLink!}
+                        onChange={onChangeBlogHandler}
+                    />
+                </div>
+                <div className="flex items-start">
+                    <label className="pl-6 font-medium text-zinc-500 relative w-[177px] flex items-center justify-start">
+                        Github
+                    </label>
+                    <Input
+                        type="url"
+                        width={500}
+                        size="big"
+                        placeholder="http://"
+                        value={basicInfo.githubLink!}
+                        onChange={onChangeGithubHandler}
+                    />
+                </div>
+            </div>
+        </main>
     );
 };
 
