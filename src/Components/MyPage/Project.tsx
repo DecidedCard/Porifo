ee475b05ec3b153eafd7b4d7e0b073e5c2e550fb
DecidedCard <heajun35@gmail.com>
2024-04-07 19:05:24 +0900

    Style: mypage ui 작성

--- src/Components/MyPage/Project.tsx
+++ src/Components/MyPage/Project.tsx
@@ -2,10 +2,13 @@
 
 import useProject from "@/hooks/mypage/useProject";
 import Image from "next/image";
+import Input from "../Commen/Input";
+import Button from "../Commen/Button";
 
 const Project = () => {
     const {
         project,
+        fileRef,
         startDate,
         endDate,
         onChangeProjectName,
@@ -20,26 +23,56 @@ const Project = () => {
     } = useProject();
 
     return (
-        <>
+        <main className="bg-white rounded-2xl mt-10 ml-9 w-[720px] pb-20">
             <div>
-                <div>
-                    <label>프로젝트 이름: </label>
-                    <input type="text" value={project.name} onChange={onChangeProjectName} />
+                <h1 className="flex items-center justify-start pl-6 pt-6 text-2xl font-bold tracking-wider">
+                    프로젝트
+                </h1>
+                <h2 className="pl-6 text-xl font-medium text-left relative flex items-center justify-start">
+                    프로젝트 정보
+                </h2>
+
+                <div className="flex">
+                    <label className="pl-6 font-medium text-zinc-500 w-[177px]">기본정보</label>
+                    <Input
+                        type="text"
+                        width={500}
+                        size="big"
+                        placeholder="프로젝트명"
+                        value={project.name}
+                        onChange={onChangeProjectName}
+                    />
                 </div>
-                <div>
-                    {/* 사진 제한 5장 */}
+                <div className="flex">
+                    {/* 사진 제한 3장 */}
+                    <p className="pl-6 font-medium text-zinc-500 w-[177px]">사진 업로드 하기</p>
                     <label htmlFor="file">
-                        <div>사진 업로드하기</div>
+                        <div className="flex items-center justify-center border border-solid border-zinc-300 w-[500px] h-[200px] cursor-pointer">
+                            {project.images.length !== 0 ? (
+                                project.images.map((item, idx) => {
+                                    return (
+                                        <Image key={idx} src={item} alt="프로젝트 미리보기" width={100} height={100} />
+                                    );
+                                })
+                            ) : (
+                                <div className="flex items-center justify-center w-10 h-10 border border-solid border-zinc-300 rounded-full text-2xl text-zinc-300">
+                                    +
+                                </div>
+                            )}
+                        </div>
                     </label>
-                    <input type="file" id="file" onChange={onChangeImagesHandler} className="hidden" multiple />
+
+                    <input
+                        ref={fileRef}
+                        type="file"
+                        id="file"
+                        onChange={onChangeImagesHandler}
+                        className="hidden"
+                        multiple
+                    />
                 </div>
-                <button onClick={onClickDeleteImage}>이미지 지우기</button>
-                <div>
-                    {project.images &&
-                        project.images.map((item, idx) => {
-                            return <Image key={idx} src={item} alt="프로젝트 미리보기" width={100} height={100} />;
-                        })}
-                </div>
+                <Button color="primary" width={200} text="이미지 지우기" onClick={onClickDeleteImage} />
+
                 <div>
                     <label>소개: </label>
                     <input type="text" value={project.introduce} onChange={onChangeProjectIntroduce} maxLength={100} />
@@ -61,7 +94,7 @@ const Project = () => {
                     +
                 </div>
             </div>
-        </>
+        </main>
     );
 };
 
