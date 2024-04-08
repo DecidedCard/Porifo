8f4c0ecd1edd47316d11cb3efb090e7ce1048f6c
sangho <keumm9635@gmail.com>
2024-04-08 10:14:38 +0900

    Style: 로그인, 회원가입, 회원가입 페이지 css 기본 틀 설정

--- src/app/(nonAuthLayout)/signin/page.tsx
+++ src/app/(nonAuthLayout)/signin/page.tsx
@@ -1,22 +1,18 @@
 "use client";
 import React, { FormEvent } from "react";
 import { supabase } from "@/util/supabase/clientSupabase";
-import useInput from "@/hooks/useInput";
 import { useRouter } from "next/navigation";
-import signInWithSocial from "@/util/sign/socialLogin";
+
+import useInput from "@/hooks/useInput";
 import Input from "@/Components/Commen/Input";
 import Button from "@/Components/Commen/Button";
+import SocialSign from "@/Components/Sign/SocialSign";
 
 const SignIn = () => {
     const [email, onChangeEmailHandler] = useInput();
     const [password, onChangePasswordHandler] = useInput();
     const router = useRouter();
-
-    const queryParams = {
-        access_type: "offline",
-        prompt: "consent",
-    };
-
+    const findPassword = () => {};
     const onSubmitLoginUser = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const hasAllInput = email.trim() !== "" && password.trim() !== "";
@@ -42,57 +38,48 @@ const SignIn = () => {
     return (
         <main>
             <div className="flex py-44 items-center justify-center bg-hihigray relative">
-                <div className="p-10 w-[600px] h-[500px] bg-white flex justify-center flex-col">
+                <div className="rounded p-10 w-[500px] h-[600px] bg-white flex justify-center flex-col">
                     <div className="flex justify-center">
-                        <img src="porifo.svg" />
+                        <img src="formLogo.svg" />
                     </div>
                     <form onSubmit={onSubmitLoginUser}>
-                        <div className="flex w-fit h-fit flex-col">
+                        <div className="flex mx-auto w-fit h-fit flex-col">
                             <label className="my-4">이메일</label>
                             <Input
                                 type="email"
                                 pattern="[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.]+[a-zA-Z]+[.]*[a-zA-Z]*"
                                 placeholder="이메일을 작성해주세요"
                                 onChange={onChangeEmailHandler}
-                                width={500}
+                                width={350}
                                 color="black"
                                 size="big"
                             />
                         </div>
-                        <div className="flex flex-col">
+                        <div className="mx-auto w-fit h-fit flex flex-col">
                             <label className="mt-8 mb-4">비밀번호</label>
                             <Input
                                 type="password"
                                 placeholder="비밀번호를 작성해주세요"
                                 onChange={onChangePasswordHandler}
-                                width={500}
+                                width={350}
                                 color="black"
                                 size="big"
                             />
                         </div>
+                        <div className="mx-9 my-4 text-slate-400 float-right" onClick={findPassword}>
+                            비밀번호 찾기 ►
+                        </div>
                         <div className="w-fit mt-8 mb-6 mx-auto">
-                            <Button text="로그인" border="none" color="primary" size="m" width={250} />
+                            <Button text="로그인" border="none" color="primary" size="m" width={350} />
                         </div>
                     </form>
-                    <div>간편하게 시작하기</div>
-                    <div className="w-48 my-5 justify-between mx-auto flex space-x-4">
-                        <img
-                            onClick={() => signInWithSocial("google", queryParams)}
-                            className="rounded-2xl shrink-0 w-156 h-136 relative"
-                            src="google.svg"
-                        />
-                        <img
-                            onClick={() => signInWithSocial("kakao")}
-                            className="rounded-2xl shrink-0 w-156 h-136 relative"
-                            src="kakao.svg"
-                        />
-                        <img
-                            onClick={() => signInWithSocial("github")}
-                            className="rounded-2xl shrink-0 w-156 h-136 relative"
-                            src="github.svg"
-                        />
+                    <SocialSign />
+                    <div className="mx-auto">
+                        아직 포리포의 회원이 아니신가요?{" "}
+                        <a href="/signup_method" className=" ml-3 underline">
+                            회원가입
+                        </a>
                     </div>
-                    <div>아직 포리포의 회원이 아니신가요?</div>
                 </div>
             </div>
         </main>
---
+++ src/app/(nonAuthLayout)/signup_method/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import SocialSign from "@/Components/Sign/SocialSign";
+import Button from "@/Components/Commen/Button";
+import { useRouter } from "next/navigation";
+
+const SignUp_Method = () => {
+    const router = useRouter();
+    const emailSignUp = () => router.push("/signup");
+
+    return (
+        <main>
+            <div className="flex py-44 items-center justify-center bg-hihigray relative">
+                <div className="rounded p-10 w-[500px] h-[400px] bg-white flex justify-center flex-col">
+                    <div className="flex justify-center ">
+                        <img src="formLogo.svg" />
+                    </div>
+                    <SocialSign />
+                    <h1 className={`${baseClass} ${afterClass} ${beforeClass}`}> 또는</h1>
+                    <div className="w-fit mt-8 mb-6 mx-auto">
+                        <Button onClick={emailSignUp} text="이메일로 회원가입" size="m" width={350} />
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default SignUp_Method;
+
+const baseClass = "mx-auto my-8 flex basis-full justify-center items-center size-12 text-gray-300";
+
+const afterClass =
+    "after:bg-black after:content-[''] after:leading-[0px] after:text-[0px] after:mx-16 after:my-0 after:grow after:h-1";
+
+const beforeClass =
+    "before:bg-black before:content-[''] before:leading-[0px] before:text-[0px] before:mx-16 before:my-0 before:grow before:h-1";
