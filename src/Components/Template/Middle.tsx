09875ec0a2a05b14345a0d2a9db7c76d5ed93f7d
leejieun2023 <lge9380@naver.com>
2024-04-09 21:42:41 +0900

    [Feat:#53] - pull dev 전 커밋

--- src/Components/Template/Middle.tsx
+++ src/Components/Template/Middle.tsx
@@ -1,19 +1,21 @@
 const Middle = () => {
     return (
         <main>
-            
+
             <div className="my-10 flex flex-row items-start justify-start self-stretch shrink-0 relative">
-                
+
                 <div className="flex flex-col items-start justify-start self-stretch shrink-0 relative" >
                     <p className="font-bold text-[22px] w-[400px] h-[214px] relative">
                         기술스택
                     </p>
+                    <div className="bg-deepgray w-[304px] h-[1px] my-5"></div>
                 </div>
 
                 <div className="flex flex-col gap-2 items-start justify-start relative">
                     <p className="font-bold text-[22px] w-[400px] h-[214px] text-left relative">
                         자기소개
                     </p>
+                    <div className="bg-deepgray w-[304px] h-[1px] my-5"></div>
                     <div className="flex flex-row gap-2 items-start justify-start self-stretch shrink-0 relative">
 
                     </div>
