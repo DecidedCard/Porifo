c838958ec4b41e861b0652daf7f94ffcc977373c
sangho <keumm9635@gmail.com>
2024-04-10 14:36:01 +0900

    [Style: #70] - 컴포넌트 button과 input에 기능 추가 및 페이지 css 추가

--- src/Components/Sign/SignValidate.tsx
+++ src/Components/Sign/SignValidate.tsx
@@ -1,35 +1,59 @@
 import React from "react";
 
 type PropsType = {
+    password: string;
     wordRegValid: boolean;
     numberRegValid: boolean;
     specialRegValid: boolean;
     lengthRegValid: boolean;
 };
 
-const SignValidate = ({ lengthRegValid, numberRegValid, wordRegValid, specialRegValid }: PropsType) => {
+const SignValidate = ({ password, lengthRegValid, numberRegValid, wordRegValid, specialRegValid }: PropsType) => {
+    const regValidate = lengthRegValid && numberRegValid && wordRegValid && specialRegValid;
+
     return (
         <div className=" flex justify-start ml-9 mb-1 -mt-5">
-            {lengthRegValid ? (
-                <p className="text-success text-center text-[11px]">✓ 8자리 이상</p>
+            {password ? (
+                <>
+                    {regValidate ? (
+                        <>
+                            <p className="text-success text-center text-[11px]">✓ 8자리 이상</p>
+                            <p className="text-success text-center text-[11px] ml-2">✓ 숫자</p>
+                            <p className="text-success text-center text-[11px] ml-2">✓ 영문</p>
+                            <p className="text-success text-center text-[11px] ml-2">✓ 특수 문자</p>
+                        </>
+                    ) : (
+                        <>
+                            {lengthRegValid ? (
+                                <p className="text-success text-center text-[11px]">✓ 8자리 이상</p>
+                            ) : (
+                                <p className="text-gray2 text-center text-[11px]">✓ 8자리 이상</p>
+                            )}
+                            {numberRegValid ? (
+                                <p className="text-success text-center text-[11px] ml-2">✓ 숫자</p>
+                            ) : (
+                                <p className="text-gray2 text-center text-[11px] ml-2">✓ 숫자</p>
+                            )}
+                            {wordRegValid ? (
+                                <p className="text-success text-center text-[11px] ml-2">✓ 영문</p>
+                            ) : (
+                                <p className="text-gray2 text-center text-[11px] ml-2">✓ 영문</p>
+                            )}
+                            {specialRegValid ? (
+                                <p className="text-success text-center text-[11px] ml-2">✓ 특수 문자</p>
+                            ) : (
+                                <p className="text-gray2 text-center text-[11px] ml-2">✓ 특수 문자</p>
+                            )}
+                        </>
+                    )}
+                </>
             ) : (
-                <p className="text-gray2 text-center text-[11px]">✓ 8자리 이상</p>
-            )}
-            {numberRegValid ? (
-                <p className="text-success text-center text-[11px]">✓ 숫자</p>
-            ) : (
-                <p className="text-gray2 text-center text-[11px]">✓ 숫자</p>
-            )}
-
-            {wordRegValid ? (
-                <p className="text-success text-center text-[11px]">✓ 영문</p>
-            ) : (
-                <p className="text-gray2 text-center text-[11px]">✓ 영문</p>
-            )}
-            {specialRegValid ? (
-                <p className="text-success text-center text-[11px]">✓ 특수 문자</p>
-            ) : (
-                <p className="text-gray2 text-center text-[11px]">✓ 특수 문자</p>
+                <>
+                    <p className="text-gray2 text-center text-[11px]">✓ 8자리 이상</p>
+                    <p className="text-gray2 text-center text-[11px] ml-2">✓ 숫자</p>
+                    <p className="text-gray2 text-center text-[11px] ml-2">✓ 영문</p>
+                    <p className="text-gray2 text-center text-[11px] ml-2">✓ 특수 문자</p>
+                </>
             )}
         </div>
     );
