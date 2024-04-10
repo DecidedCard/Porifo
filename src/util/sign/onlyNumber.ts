5b3ac326a1ea78102da5d4f03cf3c2ac64ef1c83
sangho <keumm9635@gmail.com>
2024-04-10 20:21:00 +0900

    이름변경

--- src/util/sign/onlyNumber.ts
+++ src/util/sign/onlyNumber.ts
@@ -3,7 +3,7 @@ type OnlyNumber = {
     setInputValue: React.Dispatch<React.SetStateAction<string>>;
 };
 
-export const onlyNumber = ({ e, setInputValue }: OnlyNumber) => {
+export const onlyUseNumber = ({ e, setInputValue }: OnlyNumber) => {
     const { value } = e.target;
     const onlyNumber = value.replace(/[^0-9]/g, "");
     setInputValue(onlyNumber);
