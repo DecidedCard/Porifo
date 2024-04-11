1729be3f06eb68f90337d463b5a394f5274c74e7
CodHan <qpscm662@naver.com>
2024-04-11 20:54:36 +0900

    [Feat:#69]- 한줄소개 넘치면 ... 처리

--- src/Components/Community/Carousel.tsx
+++ src/Components/Community/Carousel.tsx
@@ -42,7 +42,7 @@ const Carousel = () => {
                             className="flex flex-col gap-2  items-center justify-center shrink-0 relative"
                             style={{
                                 transform: `translateX(${translateX}px)`,
-                                transition: "transform 0.5s ease-in-out",
+                                transition: "transform 0.3s ease-in-out",
                             }}
                         >
                             <img
@@ -54,11 +54,11 @@ const Carousel = () => {
                                 src="rectangle-1150.png"
                             />
                             <div
-                                className="bg-[rgba(255,255,255,0.56)] rounded-br-2xl rounded-bl-2xl pt-4 pr-6 pb-4 pl-6 flex flex-col gap-4 items-start justify-start shrink-0 w-[550px] absolute left-0 top-[252px]"
+                                className="bg-[rgba(0,0,0,0.56)] border-2 border-solid border-sky-500 rounded-br-2xl rounded-bl-2xl pt-4 pr-6 pb-4 pl-6 flex flex-col gap-3 items-start justify-start w-[550px] absolute left-0 top-[234px]"
                                 style={{ backdropFilter: "var(--bgblur56-backdrop-filter, blur(28px))" }}
                             >
-                                <div className=" text-center font-spoqaMedium text-black font-bold text-2xl relative flex items-center justify-center">
-                                    {item.introduce}
+                                <div className=" text-center w-[500px] h-[50px]  border-2 border-solid border-rose-700  font-spoqaMedium text-white font-bold text-2xl relative flex items-center ">
+                                    <p className="truncate ...">{item.introduce}</p>
                                 </div>
                                 <div className="flex flex-row items-center justify-between self-stretch shrink-0 relative">
                                     <div className="flex flex-row gap-2 items-center justify-start flex-1 relative">
@@ -68,7 +68,7 @@ const Carousel = () => {
                                             alt={`hotDeveloper-img-${idx}`}
                                             src="rectangle0.png"
                                         />
-                                        <div className="font-spoqaMedium text-black font-bold text-center relative flex items-center justify-center">
+                                        <div className="font-spoqaMedium  text-white font-bold text-center relative flex items-center justify-center">
                                             {item.name}
                                         </div>
                                     </div>
@@ -80,18 +80,14 @@ const Carousel = () => {
                                                     src="frame-13740.svg"
                                                 />
                                             </div>
-                                            <div className="text-graytext-black text-center font-body-p7r-font-family text-body-p7r-font-size leading-body-p7r-line-height font-body-p7r-font-weight relative flex items-center justify-center">
-                                                210
-                                            </div>
+                                            <div className="text-gray">210</div>
                                         </div>
                                         <div className="flex flex-row gap-1 items-center justify-start shrink-0 relative">
                                             <img
                                                 className="shrink-0 w-6 h-6 relative overflow-visible"
                                                 src="icon-set1.svg"
                                             />
-                                            <div className="text-graytext-black text-center font-body-p7r-font-family text-body-p7r-font-size leading-body-p7r-line-height font-body-p7r-font-weight relative flex items-center justify-center">
-                                                1523
-                                            </div>
+                                            <div className="text-gray">1523</div>
                                         </div>
                                     </div>
                                 </div>
