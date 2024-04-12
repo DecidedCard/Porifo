f5f7425e3e6cb0a70a0b160e0dff4aafe5191a22
sangho <keumm9635@gmail.com>
2024-04-12 16:28:19 +0900

    [Style: #70] - socialSetting 페이지 기본 css 적용

--- src/app/(authlayout)/social_setting/page.tsx
+++ src/app/(authlayout)/social_setting/page.tsx
@@ -4,25 +4,32 @@ import React, { useState } from "react";
 import { supabase } from "@/util/supabase/clientSupabase";
 import { useRouter } from "next/navigation";
 
+import SignUpItem from "@/Components/Sign/SignUpItem";
 import useGetUser from "@/hooks/sign/useGetUser";
 import useInput from "@/hooks/useInput";
 import { signSettingValidation } from "@/util/sign/signNumber_validation";
+import Input from "@/Components/Commen/Input";
+import Button from "@/Components/Commen/Button";
+const clickSex = ["남자", "여자"];
 
 const SocialSeting = () => {
-    const [age, onChangeAgeHandler] = useInput();
-    const [phoneNumber, onChangeNumberHandler] = useInput();
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [age, setage] = useState("");
     const [sex, setSex] = useState("");
     const router = useRouter();
     useGetUser();
 
-    const onClickeMale = (e: React.MouseEvent<HTMLButtonElement>): void => {
-        e.preventDefault();
-        setSex("남자");
-    };
+    const onClickFindSex = (sex: string) => setSex(sex);
 
-    const onClickeFeMale = (e: React.MouseEvent<HTMLButtonElement>): void => {
-        e.preventDefault();
-        setSex("여자");
+    const onChangeAge = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        const onlyNumber = value.replace(/[^0-9]/g, "");
+        setage(onlyNumber);
+    };
+    const onChangePhoneNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        const onlyNumber = value.replace(/[^0-9]/g, "");
+        setPhoneNumber(onlyNumber);
     };
 
     const signUpNewUser = async (e: React.FormEvent) => {
@@ -42,24 +49,49 @@ const SocialSeting = () => {
     };
 
     return (
-        <form onSubmit={signUpNewUser}>
-            <h1>추가 정보를 입력해 주세요.</h1>
+        <div className="flex py-44 items-center justify-center bg-hihigray relative">
+            <div className="rounded p-10 w-[500px] h-[500px] bg-white flex justify-center flex-col">
+                <form onSubmit={signUpNewUser}>
+                    <h1 className="text-center">추가 정보를 입력해 주세요.</h1>
+                    <SignUpItem
+                        setLabel="나이"
+                        type="text"
+                        value={age}
+                        pattern="[0-9]{2}"
+                        maxLength={2}
+                        placeholder="나이를 입력해주세요"
+                        onChangeHandler={onChangeAge}
+                    />
+                    <SignUpItem
+                        setLabel="핸드폰 번호"
+                        type="text"
+                        placeholder="비밀번호를 작성해주세요"
+                        onChangeHandler={onChangePhoneNumber}
+                    />
 
-            <div>
-                <label>나이</label>
-                <input placeholder="나이를 입력해주세요" maxLength={2} onChange={onChangeAgeHandler} />
+                    <p className="mx-9">성별</p>
+                    <div className="mx-9 mt-[9px] mb-8 h-fit flex flex-row ">
+                        {clickSex.map((item: string, idx: number) => {
+                            return (
+                                <div key={idx} className="flex flex-row my-auto mr-4">
+                                    <label className="mr-1">{item}</label>
+                                    <input
+                                        type="radio"
+                                        name="sex"
+                                        value={item}
+                                        id={item}
+                                        onClick={() => onClickFindSex(item)}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <div className="w-[350px] mt-8 mb-6 mx-auto">
+                        <Button text="모두 입력해 주세요" border="none" size="m" color="primary" />
+                    </div>
+                </form>
             </div>
-            <div>
-                <label>핸드폰 번호</label>
-                <input onChange={onChangeNumberHandler} maxLength={11} placeholder="-없이 작성해주세요" />
-            </div>
-            <div>
-                <label>성별</label>
-                <button onClick={onClickeMale}>남자</button>
-                <button onClick={onClickeFeMale}>여자</button>
-            </div>
-            <button>모두 작성해 주세요.</button>
-        </form>
+        </div>
     );
 };
 
