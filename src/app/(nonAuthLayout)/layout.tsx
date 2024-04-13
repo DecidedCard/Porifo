16fd8faef3e1d3b831f93b9b073d0aa050d4d69a
sangho <keumm9635@gmail.com>
2024-04-13 21:58:33 +0900

    nonAuthLayout 속 layout 주석 삭제

--- src/app/(nonAuthLayout)/layout.tsx
+++ src/app/(nonAuthLayout)/layout.tsx
@@ -5,15 +5,15 @@ import { useRouter } from "next/navigation";
 import React, { PropsWithChildren } from "react";
 
 const NonAuthLayout = ({ children }: PropsWithChildren) => {
-    // const { isFetching, isError } = useLoginCheck();
-    // const router = useRouter();
-    // if (isFetching) {
-    //     return <>로딩중입니다...</>;
-    // }
-    // if (!isFetching && !isError) {
-    //     alert("로그인후 이용은 불가능합니다.");
-    //     router.replace("/mypage");
-    // }
+    const { isFetching, isError } = useLoginCheck();
+    const router = useRouter();
+    if (isFetching) {
+        return <>로딩중입니다...</>;
+    }
+    if (!isFetching && !isError) {
+        alert("로그인후 이용은 불가능합니다.");
+        router.replace("/mypage");
+    }
 
     return <div>{children}</div>;
 };
