1b4565d583f8cbad18dd5a6453df999be5f0789d
leejieun2023 <lge9380@naver.com>
2024-04-14 13:48:05 +0900

    [Style:#95] - 기존 템플릿 다음 버튼 스타일 수정

--- src/Components/MainPage/Recommendation.tsx
+++ src/Components/MainPage/Recommendation.tsx
@@ -57,18 +57,143 @@ const Recommendation = () => {
                     </div>
 
                     {/* 2번 */}
-                    <div className="flex pl-6 pr-6 pt-4 bg-white rounded-2xl border-solid border border-gray w-[302px] h-[210px]">
-
+                    <div className="flex bg-white rounded-2xl border-solid border border-gray w-[302px] h-[210px]">
+                        <div className="flex flex-col gap-2 items-start justify-start flex-1 relative">
+                            <img
+                                className="rounded-2xl  w-[302px] h-[210px]"
+                                style={{
+                                    background: "linear-gradient(to left, #d9d9d9, #d9d9d9)",
+                                    objectFit: "cover",
+                                }}
+                                src="rectangle-1153.png"
+                            />
+                            <div className="flex flex-row items-center justify-between self-stretch">
+                                <div className="flex flex-row gap-2 items-center justify-start flex-1">
+                                    <img
+                                        className="rounded-pull w-8 h-8"
+                                        src="rectangle0.png"
+                                    />
+                                    <div className="text-centerflex items-center justify-center">
+                                        최원장
+                                    </div>
+                                </div>
+                                <div className="flex flex-row gap-4 items-center justify-end">
+                                    <div className="flex flex-row gap-1 items-center justify-start">
+                                        <div className="shrink-0 w-6 h-6 relative">
+                                            <img
+                                                className="w-[100%] h-[100%] absolute right-[0%] left-[0%] bottom-[0%] top-[0%] overflow-visible"
+                                                src="frame-13740.svg"
+                                            />
+                                        </div>
+                                        <div className="text-center flex items-center justify-center">
+                                            210
+                                        </div>
+                                    </div>
+                                    <div className="flex flex-row gap-1 items-center justify-start shrink-0 relative">
+                                        <img
+                                            className="shrink-0 w-6 h-6 relative overflow-visible"
+                                            src="icon-set1.svg"
+                                        />
+                                        <div className="text-center flex items-center justify-center">
+                                            1523
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     {/* 3번 */}
-                    <div className="flex pl-6 pr-6 pt-4 bg-white rounded-2xl border-solid border border-gray w-[302px] h-[210px]">
-
+                    <div className="flex bg-white rounded-2xl border-solid border border-gray w-[302px] h-[210px]">
+                        <div className="flex flex-col gap-2 items-start justify-start flex-1 relative">
+                            <img
+                                className="rounded-2xl  w-[302px] h-[210px]"
+                                style={{
+                                    background: "linear-gradient(to left, #d9d9d9, #d9d9d9)",
+                                    objectFit: "cover",
+                                }}
+                                src="rectangle-1153.png"
+                            />
+                            <div className="flex flex-row items-center justify-between self-stretch">
+                                <div className="flex flex-row gap-2 items-center justify-start flex-1">
+                                    <img
+                                        className="rounded-pull w-8 h-8"
+                                        src="rectangle0.png"
+                                    />
+                                    <div className="text-centerflex items-center justify-center">
+                                        권혁우
+                                    </div>
+                                </div>
+                                <div className="flex flex-row gap-4 items-center justify-end">
+                                    <div className="flex flex-row gap-1 items-center justify-start">
+                                        <div className="shrink-0 w-6 h-6 relative">
+                                            <img
+                                                className="w-[100%] h-[100%] absolute right-[0%] left-[0%] bottom-[0%] top-[0%] overflow-visible"
+                                                src="frame-13740.svg"
+                                            />
+                                        </div>
+                                        <div className="text-center flex items-center justify-center">
+                                            210
+                                        </div>
+                                    </div>
+                                    <div className="flex flex-row gap-1 items-center justify-start shrink-0 relative">
+                                        <img
+                                            className="shrink-0 w-6 h-6 relative overflow-visible"
+                                            src="icon-set1.svg"
+                                        />
+                                        <div className="text-center flex items-center justify-center">
+                                            1523
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
                     {/* 4번 */}
-                    <div className="flex pl-6 pr-6 pt-4 bg-white rounded-2xl border-solid border border-gray w-[302px] h-[210px]">
-
+                    <div className="flex bg-white rounded-2xl border-solid border border-gray w-[302px] h-[210px]">
+                        <div className="flex flex-col gap-2 items-start justify-start flex-1 relative">
+                            <img
+                                className="rounded-2xl  w-[302px] h-[210px]"
+                                style={{
+                                    background: "linear-gradient(to left, #d9d9d9, #d9d9d9)",
+                                    objectFit: "cover",
+                                }}
+                                src="rectangle-1153.png"
+                            />
+                            <div className="flex flex-row items-center justify-between self-stretch">
+                                <div className="flex flex-row gap-2 items-center justify-start flex-1">
+                                    <img
+                                        className="rounded-pull w-8 h-8"
+                                        src="rectangle0.png"
+                                    />
+                                    <div className="text-centerflex items-center justify-center">
+                                        윤창식
+                                    </div>
+                                </div>
+                                <div className="flex flex-row gap-4 items-center justify-end">
+                                    <div className="flex flex-row gap-1 items-center justify-start">
+                                        <div className="shrink-0 w-6 h-6 relative">
+                                            <img
+                                                className="w-[100%] h-[100%] absolute right-[0%] left-[0%] bottom-[0%] top-[0%] overflow-visible"
+                                                src="frame-13740.svg"
+                                            />
+                                        </div>
+                                        <div className="text-center flex items-center justify-center">
+                                            210
+                                        </div>
+                                    </div>
+                                    <div className="flex flex-row gap-1 items-center justify-start shrink-0 relative">
+                                        <img
+                                            className="shrink-0 w-6 h-6 relative overflow-visible"
+                                            src="icon-set1.svg"
+                                        />
+                                        <div className="text-center flex items-center justify-center">
+                                            1523
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
