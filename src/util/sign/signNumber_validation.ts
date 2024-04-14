792c5bbab848687da0d175680c3b18ec3848fbe8
DecidedCard <heajun35@gmail.com>
2024-04-15 01:32:14 +0900

    [Feat:#98] - 연락처 입력 제거

--- src/util/sign/signNumber_validation.ts
+++ src/util/sign/signNumber_validation.ts
@@ -1,35 +1,23 @@
 type SignUpValidationType = {
-    phoneNumber: string;
     birthDate: string;
     email: string;
     password: string;
 };
 
 type SignSettingValidationType = {
-    phoneNumber: string;
     sex: string;
     birthDate: string;
 };
 
-export const signUpValidation = ({ phoneNumber, birthDate, email, password }: SignUpValidationType) => {
-    if (phoneNumber.length !== 11) {
-        alert("핸드폰 번호를 정확히 입력해 주세요.");
-        return;
-    }
-
+export const signUpValidation = ({ birthDate, email, password }: SignUpValidationType) => {
     if (email.trim() === "" && password.trim() === "" && birthDate === "") {
         alert("정확한 값이 입력되지 않았습니다.");
         return;
     }
 };
 
-export const signSettingValidation = ({ phoneNumber, birthDate, sex }: SignSettingValidationType) => {
-    if (phoneNumber.length !== 11) {
-        alert("핸드폰 번호를 정확히 입력해 주세요.");
-        return;
-    }
-
-    const hasAllInput = sex == "" && phoneNumber.trim() !== "" && birthDate === "";
+export const signSettingValidation = ({ birthDate, sex }: SignSettingValidationType) => {
+    const hasAllInput = sex == "" && birthDate === "";
 
     if (hasAllInput) {
         alert("모두 입력해 주세요");
