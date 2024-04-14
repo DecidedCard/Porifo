c78ab5efc21aef26d380aef6f0ecbae59ed9b53d
DecidedCard <heajun35@gmail.com>
2024-04-15 02:33:02 +0900

    [Style:#98] - 헤더 css 수정

--- src/util/supabase/supabase_user.ts
+++ src/util/supabase/supabase_user.ts
@@ -2,12 +2,15 @@ import { supabase } from "./clientSupabase";
 
 export const userData = async () => {
     try {
-        const { data, error } = await supabase.auth.getUser();
+        const {
+            data: { user },
+            error,
+        } = await supabase.auth.getUser();
         if (error) {
             console.error(error);
             throw new Error("로그인을 해주시기 바랍니다.");
         }
-        return data;
+        return user;
     } catch (error) {
         Promise.reject(error);
     }
