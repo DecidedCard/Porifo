fdb056c299809016c79b193465cb36b259456c32
Cod-Han <qpscm662@naver.com>
2024-04-14 19:52:47 +0900

    [Feat:#96] - 댓글 불러오기 없을 시 없다고 표시

--- src/Components/DetailPage/Comments.tsx
+++ src/Components/DetailPage/Comments.tsx
@@ -25,30 +25,41 @@ const Comments = () => {
                 <CommentInput user={user} id={id} />
                 <div className="border-[1px] border-solid border-gray2" />
                 {/* 댓글리스트 */}
-                <div className="flex gap-3">
-                    {/* profileImage */}
-                    <img className="rounded-[50px] object-cover" src="rectangle1.png" />
-                    <div className="flex flex-col gap-1 items-start justify-start flex-1">
-                        <div className="flex flex-row gap-2 items-center justify-start">
-                            <div className="text-graytext-5 text-left font-body-p8m-font-family text-body-p8m-font-size leading-body-p8m-line-height font-body-p8m-font-weight relative flex items-center justify-start">
-                                {/* user_name */}
-                                Dora
+                {data?.length === 0 ? (
+                    <div>댓글이 없어요</div>
+                ) : (
+                    data?.map((item) => {
+                        return (
+                            <div className="flex gap-3">
+                                {/* profileImage */}
+                                <img
+                                    className="rounded-[50px] w-10 h-10 object-cover"
+                                    src="https://heurm-tutorial.vlpt.us/images/default_thumbnail.png"
+                                />
+                                <div className="flex flex-col gap-1 flex-1 ">
+                                    <div className="flex flex-row gap-2 items-center">
+                                        <span>
+                                            {/* user_name */}
+                                            {item.user_name}
+                                        </span>
+                                        <span>
+                                            {/* 몇일됬는지 */}
+                                            {item.created_at}
+                                        </span>
+                                    </div>
+                                    <div className="flex gap-2">
+                                        <span>
+                                            {/* comment */}
+                                            {item.comment}
+                                        </span>
+                                    </div>
+                                </div>
+                                {/* 삭제버튼 */}
+                                <img className="cursor-pointer" src="grayClose.svg" />
                             </div>
-                            <div className="text-graytext-4 text-left font-body-p9r-font-family text-body-p9r-font-size leading-body-p9r-line-height font-body-p9r-font-weight relative flex items-center justify-start">
-                                {/* 몇일됬는지 */}
-                                1일 전
-                            </div>
-                        </div>
-                        <div className="flex flex-row gap-2 items-start justify-start shrink-0 relative">
-                            <div className="text-graytext-5 text-left font-body-p7r-font-family text-body-p7r-font-size leading-body-p7r-line-height font-body-p7r-font-weight relative flex items-center justify-start">
-                                {/* comment */}
-                                So nice!
-                            </div>
-                        </div>
-                    </div>
-                    {/* 삭제버튼 */}
-                    <img className="cursor-pointer" src="grayClose.svg" />
-                </div>
+                        );
+                    })
+                )}
             </div>
         </>
     );
