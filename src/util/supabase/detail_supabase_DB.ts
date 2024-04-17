8b8d96c9db7dbe15453d3861424440e89f2c6646
CodHan <qpscm662@naver.com>
2024-04-17 19:40:03 +0900

    [Refactor:#151] - detail supabase 로직 수정

--- src/util/supabase/detail_supabase_DB.ts
+++ src/util/supabase/detail_supabase_DB.ts
@@ -3,20 +3,18 @@ import { supabase } from "./clientSupabase";
 
 export const getDetailData = async (col: { id: string; value: string }) => {
     try {
-        const { data: portfolioInfo, error: er } = await supabase
-            .from("portfolioInfo")
-            .select("*")
-            .eq(col.id, col.value);
-        if (er) {
-            console.error(er);
-            throw new Error("에러가 발생했습니다.");
-        }
-        const { id, viewCnt }: PortfolioInfo = portfolioInfo[0];
+        const { data: portfolioInfo } = await supabase.from("portfolioInfo").select("*").eq(col.id, col.value);
+
+        const { id, viewCnt }: PortfolioInfo = portfolioInfo![0];
 
         const { error } = await supabase
             .from("portfolioInfo")
             .update({ viewCnt: viewCnt! + 1 })
             .eq("id", id);
+        if (error) {
+            console.error(error);
+            throw new Error("에러가 발생했습니다.");
+        }
 
         return portfolioInfo;
     } catch (error) {
