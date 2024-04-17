fc40fb07c5229f96d57ac39572b53d2bc366faa9
sangho <keumm9635@gmail.com>
2024-04-17 17:37:54 +0900

    send 폴더의 route 주석 해제

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,21 +1,21 @@
-// import SignEmailTemplate from "@/Components/Sign/SignEmailTemplate";
-// import { Resend } from "resend";
+import SignEmailTemplate from "@/Components/Sign/SignEmailTemplate";
+import { Resend } from "resend";
 
-// const resend = new Resend(`${process.env.RESEND_API_KEY}`);
+const resend = new Resend(`${process.env.RESEND_API_KEY}`);
 
-// export async function POST() {
-//     try {
-//         const { data, error } = await resend.emails.send({
-//             from: "Acme <onboarding@resend.dev>",
-//             to: ["delivered@resend.dev"],
-//             subject: "Hello world",
-//             react: SignEmailTemplate({ name: "원숭이" }),
-//         });
-//         if (error) {
-//             return Response.json({ error });
-//         }
-//         return Response.json(data);
-//     } catch (error) {
-//         return Response.json({ error });
-//     }
-// }
+export async function POST() {
+    try {
+        const { data, error } = await resend.emails.send({
+            from: "Acme <onboarding@resend.dev>",
+            to: ["delivered@resend.dev"],
+            subject: "Hello world",
+            react: SignEmailTemplate({ name: "원숭이" }),
+        });
+        if (error) {
+            return Response.json({ error });
+        }
+        return Response.json(data);
+    } catch (error) {
+        return Response.json({ error });
+    }
+}
