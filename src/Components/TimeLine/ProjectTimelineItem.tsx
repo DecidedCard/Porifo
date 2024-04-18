3e81a1925aa78e6ae161aacbfe99f554502f17a4
DecidedCard <heajun35@gmail.com>
2024-04-18 20:24:12 +0900

    [Feat] - 템플릿 링크 태그 작성 및 조건 작성

--- src/Components/TimeLine/ProjectTimelineItem.tsx
+++ src/Components/TimeLine/ProjectTimelineItem.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface TimelineItemProps {
     name: string;
@@ -22,7 +23,6 @@ const TimelineItem: React.FC<TimelineItemProps> = ({
     githubLink,
     projectCount,
 }) => {
-
     return (
         <li className="mb-10 pl-5 flex flex-col items-center justify-center">
             {projectCount > 1 && (
@@ -31,16 +31,17 @@ const TimelineItem: React.FC<TimelineItemProps> = ({
             <div className="flex flex-col w-[804px]">
                 {/* 이미지 영역 - 이미지가 있을 경우에만 렌더링 */}
                 <div className="flex flex-row">
-                    {images.map((image, index) => (
-                        <Image
-                            key={index}
-                            src={image}
-                            alt="프로젝트"
-                            className="flex flex-row w-[263px] h-[198px] rounded-2xl mr-2"
-                            width={800}
-                            height={800}
-                        />
-                    ))}
+                    {images &&
+                        images.map((image, index) => (
+                            <Image
+                                key={index}
+                                src={image}
+                                alt="프로젝트"
+                                className="flex flex-row w-[263px] h-[198px] rounded-2xl mr-2"
+                                width={800}
+                                height={800}
+                            />
+                        ))}
                 </div>
 
                 {/* 제목과 날짜를 포함하는 영역 */}
@@ -53,10 +54,21 @@ const TimelineItem: React.FC<TimelineItemProps> = ({
                     {/* 설명과 상세 정보를 포함하는 영역 */}
                     <div className="flex flex-col mt-2 w-[804px]">
                         <p className="text-[14px] font-normal mb-2 text-gray3">{introduce}</p>
-                        <div className="flex items-center text-gray3 mt-2">
-                            <Image src="/assets/image/link.svg" alt="link" width={24} height={24} className="mr-1" />
-                            <p className="text-[12px]">{githubLink}</p>
-                        </div>
+                        {githubLink && (
+                            <div className="flex items-center text-gray3 mt-2">
+                                <Image
+                                    src="/assets/image/link.svg"
+                                    alt="link"
+                                    width={24}
+                                    height={24}
+                                    className="mr-1"
+                                />
+                                <Link href={githubLink}>
+                                    <p className="text-[12px]">{githubLink}</p>
+                                </Link>
+                            </div>
+                        )}
+
                         {deployLink && (
                             <div className="flex items-center text-gray3 mt-2">
                                 <Image
@@ -66,7 +78,9 @@ const TimelineItem: React.FC<TimelineItemProps> = ({
                                     height={24}
                                     className="mr-1"
                                 />
-                                <p className="text-[12px]">{deployLink}</p>
+                                <Link href={deployLink}>
+                                    <p className="text-[12px]">{deployLink}</p>
+                                </Link>
                             </div>
                         )}
                     </div>
