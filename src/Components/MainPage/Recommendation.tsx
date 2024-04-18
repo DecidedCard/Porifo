fab2dd275fc4ab22d9883db59ace4bbf79c082bf
leejieun2023 <lge9380@naver.com>
2024-04-18 11:27:31 +0900

    [Style:#160] - 메인페이지 추천개발자 컴포넌트 css 수정

--- src/Components/MainPage/Recommendation.tsx
+++ src/Components/MainPage/Recommendation.tsx
@@ -24,96 +24,86 @@ const Recommendation = () => {
 
     return (
         <main>
-            <div className="flex flex-col justify-center items-center h-[466px] w-screen bg-hihigray">
-                <div>
-                    <div className="flex flex-col gap-6 items-start justify-start">
-                        <p className="flex items-start justify-start font-spoqaBold leading-normal font-bold text-[40px] mb-14">
-                            포리포 추천, HOT🔥 개발자
-                        </p>
-                    </div>
+            <div className="flex flex-col items-center justify-center h-[466px] w-screen bg-hihigray">
+                <div className="flex flex-col gap-6 items-start">
+                    <p className="font-spoqaBold text-[40px] font-bold mb-14">
+                        포리포 추천, HOT🔥 개발자
+                    </p>
+                </div>
 
-                    <div className="flex flex-row gap-6 items-start justify-start">
-                        {data?.slice(0, 4).map((developer: any, idx) => (
-                            <React.Fragment key={idx}>
-                                <div className="flex rounded-2xl w-[302px] h-[225px] overflow-hidden">
-                                    <div className="flex flex-row gap-2 items-center justify-center">
+                <div className="flex flex-row gap-6">
+                    {data?.slice(0, 4).map((developer: any, idx) => (
+                        <React.Fragment key={idx}>
+                            <div className="rounded-2xl w-[302px] h-[225px] overflow-hidden">
+                                <div className="flex items-center justify-center">
+                                    <div className="relative">
+                                        <Image
+                                            className="rounded-2xl ease-in-out duration-300 hover:scale-105"
+                                            src={developer.profileImage || ""}
+                                            alt="썸네일"
+                                            width={302}
+                                            height={210}
+                                            style={{ objectFit: "cover" }}
+                                        />
+                                        <div
+                                            className="absolute top-[136px] w-full h-[90px] bg-[#0000008F] rounded-bl-2xl rounded-br-2xl px-6"
+                                            style={{ backdropFilter: "var(--bgblur56-backdrop-filter, blur(28px))" }}>
+                                            <div className="flex items-center mt-5">
+                                                <div className="flex flex-col gap-4 flex-1 text-sm">
+                                                    <p className="text-white">{developer.oneLineIntroduce.length > 20
+                                                        ? `${developer.oneLineIntroduce.substring(0, 20)}...`
+                                                        : developer.oneLineIntroduce}</p>
 
-                                        <div className="flex flex-col relative">
-                                            <Image
-                                                className="rounded-2xl w-[302px] h-[210px] ease-in-out duration-300 hover:scale-105"
-                                                src={developer.profileImage || ""}
-                                                alt="썸네일"
-                                                width={302}
-                                                height={210}
-                                                style={{
-                                                    objectFit: "cover",
-                                                }}
-                                            />
-                                            <div
-                                                className="bg-[rgba(0,0,0,0.56)] rounded-br-2xl rounded-bl-2xl px-6 flex flex-col gap-3 absolute top-[134px] w-[302px] h-[90px]"
-                                                style={{ backdropFilter: "var(--bgblur56-backdrop-filter, blur(28px))" }}>
-                                                <div className="flex flex-row items-center mt-5">
-                                                    <div className="flex flex-col gap-4 items-start justify-start flex-1">
 
-                                                        <p className="text-white">{developer.oneLineIntroduce.length > 20
-                                                            ? `${developer.oneLineIntroduce.substring(0, 20)}...`
-                                                            : developer.oneLineIntroduce}</p>
-
-                                                        <div className="flex flex-row justify-between w-full">
-                                                            <div className="flex flex-row gap-2">
-                                                                <Image
-                                                                    className="rounded-full w-[20px] h-[20px]"
-                                                                    src={developer.profileImage || ""}
-                                                                    alt="프로필 이미지"
-                                                                    width={20}
-                                                                    height={20}
-                                                                    style={{
-                                                                        objectFit: "cover",
-                                                                    }}
-                                                                />
-                                                                <div className="text-center flex items-center justify-center text-white">
-                                                                    {developer.name}
-                                                                </div>
+                                                    <div className="flex justify-between w-full">
+                                                        <div className="flex items-center gap-2">
+                                                            <Image
+                                                                className="rounded-full w-5 h-5"
+                                                                src={developer.profileImage || ""}
+                                                                alt="프로필 이미지"
+                                                                width={20}
+                                                                height={20}
+                                                                style={{ objectFit: "cover" }}
+                                                            />
+                                                            <div className="text-white text-[10px] flex items-center">
+                                                                {developer.name}
                                                             </div>
-
-
-
-                                                            <div>
-                                                                {/* <Image
-                                                            src="grayEye.svg"
-                                                            alt="좋아요 아이콘"
-                                                            width={24}
-                                                            height={24}
-                                                            className="mr-2"
-                                                             /> */}
-                                                            </div>
-
-                                                            <div className="flex flex-row items-center">
-                                                                <Image
-                                                                    src="grayEye.svg"
-                                                                    alt="조회수 아이콘"
-                                                                    width={24}
-                                                                    height={24}
-                                                                    className="mr-1"
-                                                                />
-                                                                <p className="text-white">
-                                                                    {developer.viewCnt}
-                                                                </p>
-                                                            </div>
-
                                                         </div>
-                                                    </div>
 
+                                                        <div className="flex items-center ml-20 text-[10px]">
+                                                            <Image
+                                                                src="grayHeart.svg"
+                                                                alt="좋아요 아이콘"
+                                                                width={24}
+                                                                height={24}
+                                                                className="mr-1"
+                                                            />
+                                                            <p className="text-white">{developer.viewCnt}</p>
+                                                        </div>
+
+                                                        <div className="flex items-center text-[10px]">
+                                                            <Image
+                                                                src="grayEye.svg"
+                                                                alt="조회수 아이콘"
+                                                                width={24}
+                                                                height={24}
+                                                                className="mr-1"
+                                                            />
+                                                            <p className="text-white">{developer.viewCnt}</p>
+                                                        </div>
+
+                                                    </div>
                                                 </div>
+
                                             </div>
                                         </div>
-
-
                                     </div>
+
+
                                 </div>
-                            </React.Fragment>
-                        ))}
-                    </div>
+                            </div>
+                        </React.Fragment>
+                    ))}
                 </div>
             </div>
 
