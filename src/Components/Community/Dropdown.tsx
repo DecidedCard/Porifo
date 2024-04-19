523546f8eb42f291443e03eb80f84c6ed3ddc7e1
CodHan <qpscm662@naver.com>
2024-04-19 11:42:20 +0900

    [Feat:151] - 드롭다운 인기순 추가

--- src/Components/Community/Dropdown.tsx
+++ src/Components/Community/Dropdown.tsx
@@ -5,10 +5,23 @@ import { useQueryClient } from "@tanstack/react-query";
 import { QUERY_KEY } from "@/util/query_key";
 
 import useSupabaseRange from "@/hooks/useSupabaseRange";
+import useJobFilterStore from "@/store/jobFilterStore";
+
+const filterValue = [
+    {
+        value: "최신순",
+    },
+    {
+        value: "오래된 순",
+    },
+    {
+        value: "인기순",
+    },
+];
 
 const Dropdown = ({ props }: any) => {
     const { setView } = props;
-    const { setPage, setFilter } = useSupabaseRange();
+    const { setFilter } = useJobFilterStore();
 
     const queryClient = useQueryClient();
 
@@ -20,12 +33,14 @@ const Dropdown = ({ props }: any) => {
         if (filterValue === "오래된 순") {
             return refetch("오래된 순");
         }
+        if (filterValue === "인기순") {
+            return refetch("인기순");
+        }
     };
 
     //필터 옵션 변경시 refetch 함수
     const refetch = (filter: string) => {
         queryClient.removeQueries({ queryKey: [QUERY_KEY.communityPortfolio] });
-        setPage(0);
         return setFilter(filter);
     };
 
@@ -35,27 +50,18 @@ const Dropdown = ({ props }: any) => {
 
     return (
         <div className="list-none font-spoqaMedium font-bold border-solid border-2 border-t-0 border-gray2 rounded-xl ">
-            <li
-                value="최신순"
-                onClick={() => handleFilterChange("최신순")}
-                className="p-4 hover:bg-hihigray rounded-xl cursor-pointer"
-            >
-                최신순
-            </li>
-            <li
-                value="오래된 순"
-                onClick={() => handleFilterChange("오래된 순")}
-                className="p-4 hover:bg-hihigray rounded-xl cursor-pointer"
-            >
-                오래된 순
-            </li>
-            <li
-                value="인기순"
-                onClick={() => handleFilterChange("인기순")}
-                className="p-4 hover:bg-hihigray rounded-xl cursor-pointer "
-            >
-                인기순
-            </li>
+            {filterValue.map((item, idx) => {
+                return (
+                    <li
+                        key={idx}
+                        value={item.value}
+                        onClick={() => handleFilterChange(item.value)}
+                        className="p-4 hover:bg-hihigray rounded-xl cursor-pointer"
+                    >
+                        {item.value}
+                    </li>
+                );
+            })}
         </div>
     );
 };
