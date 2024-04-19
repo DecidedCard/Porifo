733888b82ed754e750e3a6c5658302aa711ada2d
CodHan <qpscm662@naver.com>
2024-04-19 14:23:27 +0900

    [Refactor:#151] - 주석제거

--- src/util/supabase/detail_supabase_DB.ts
+++ src/util/supabase/detail_supabase_DB.ts
@@ -23,21 +23,11 @@ export const getDetailData = async (col: { id: string; value: string }) => {
 };
 
 //likes
-
 export const getLikes = async (col: { id: string; value: string }) => {
     try {
         const { data: likesInfo } = await supabase.from("portfolioInfo").select("*").eq(col.id, col.value);
 
         const { likes } = likesInfo![0];
-
-        // const { error } = await supabase
-        //     .from("portfolioInfo")
-        //     .update({ viewCnt: viewCnt! + 1 })
-        //     .eq("id", id);
-        // if (error) {
-        //     console.error(error);
-        //     throw new Error("에러가 발생했습니다.");
-        // }
 
         return likes;
     } catch (error) {
