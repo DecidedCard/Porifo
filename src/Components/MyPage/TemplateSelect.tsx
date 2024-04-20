745c5b81ac225f9d6e3bf5679bc03fcf15eb10f4
DecidedCard <heajun35@gmail.com>
2024-04-20 20:42:34 +0900

    [Style] - 반응형 디자인 시도중

--- src/Components/MyPage/TemplateSelect.tsx
+++ src/Components/MyPage/TemplateSelect.tsx
@@ -18,11 +18,11 @@ const TemplateSelect = ({ onClickTemplateSelectHandler }: { onClickTemplateSelec
 
     return (
         <div className="fixed top-0 left-0 bottom-0 right-0 w-screen h-screen bg-black bg-opacity-80 z-50">
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-5 w-[932px] h-[741px] bg-white rounded-2xl">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-5 w-[932px] h-[741px] bg-white rounded-2xl sm:w-full sm:mx-auto sm:h-fit sm:overflow-auto">
                 <h2 className="w-fit h-9 mx-auto text-2xl font-bold mt-10 text-[22px]">원하는 템플릿을 선택하세요.</h2>
-                <hr className="w-[852px] h-4 text-gray2 mx-auto" />
-                <div className="flex gap-6 w-[852px] h-[549px] mx-auto">
-                    <div className="flex flex-col gap-2 rounded-2xl">
+                <hr className="w-[852px] h-4 text-gray2 mx-auto sm:w-[90%]" />
+                <div className="flex gap-6 w-[852px] h-[549px] mx-auto sm:flex-col">
+                    <div className="flex flex-col gap-2 rounded-2xl sm:ml-2">
                         <h3 className="text-lg font-medium text-gray4">Standard</h3>
                         <div className="flex flex-col items-center justify-center gap-2 w-[414px] h-[517px] bg-blue rounded-2xl">
                             <div className="w-[315px] h-[413px] rounded-2xl overflow-hidden">
@@ -46,7 +46,7 @@ const TemplateSelect = ({ onClickTemplateSelectHandler }: { onClickTemplateSelec
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-col gap-2 rounded-2xl">
+                    <div className="flex flex-col gap-2 rounded-2xl sm:ml-2">
                         <h3 className="text-lg font-medium text-gray4">Grid</h3>
                         <div className="flex flex-col items-center justify-center gap-2 w-[414px] h-[517px] bg-sky_blue rounded-2xl">
                             <div className="w-[315px] h-[413px] rounded-2xl overflow-hidden">
