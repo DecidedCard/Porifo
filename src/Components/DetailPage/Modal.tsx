2089c836b4429dc91acaedc2278de32359c5342b
CodHan <qpscm662@naver.com>
2024-04-23 00:47:58 +0900

    [Feat] - 공유하기 기능 연결

--- src/Components/DetailPage/Modal.tsx
+++ src/Components/DetailPage/Modal.tsx
@@ -82,6 +82,7 @@ const Modal = ({ isVisible, onClose, children }: any) => {
             id="wrapper"
             onClick={handleClose}
         >
+            {/* 뒤로가기 버튼 */}
             <button
                 className="bg-[rgba(255,255,255,0.80)] rounded-[999px] w-[48px] h-[48px] p-2 flex flex-row gap-2 items-center justify-center shrink-0 absolute left-[22%] top-[25px] ease-in-out duration-300 hover:bg-nonegray hover:bg-opacity-20"
                 style={{ boxShadow: "0px 4px 12px 0px rgba(0, 0, 0, 0.16)", backdropFilter: "blur(28px)" }}
@@ -89,13 +90,12 @@ const Modal = ({ isVisible, onClose, children }: any) => {
             >
                 <Image src="icon-set9.svg" alt="아이콘" width={25} height={25} />
             </button>
-            {/* 오른쪽 */}
+            {/* 오른쪽 UI */}
             <div className="absolute right-[22%] top-[25px] flex flex-col gap-[28px] ">
                 {/* 좋아요 */}
-                <div className="flex flex-col items-center justify-center gap-2">
+                <div className="flex flex-col items-center justify-center gap-2 ">
                     <button
-                        className="bg-[rgba(255,255,255,0.80)] w-[48px] h-[48px] rounded-[999px] p-2 flex flex-row gap-2 items-center justify-center ease-in-out duration-300 hover:bg-nonegray hover:bg-opacity-20"
-                        style={{ boxShadow: "0px 4px 12px 0px rgba(0, 0, 0, 0.16)", backdropFilter: "blur(28px)" }}
+                        className="bg-white w-[48px] h-[48px] rounded-[999px] p-2 flex items-center justify-center"
                         onClick={handleLikeBtn}
                     >
                         {checkLike ? (
@@ -109,8 +109,7 @@ const Modal = ({ isVisible, onClose, children }: any) => {
                 {/* 댓글 */}
                 <div className="flex flex-col items-center justify-center gap-2">
                     <button
-                        className="bg-[rgba(255,255,255,0.80)] w-[48px] h-[48px] rounded-[999px] p-2 flex flex-row gap-2 items-center justify-center ease-in-out duration-300 hover:bg-nonegray hover:bg-opacity-20"
-                        style={{ boxShadow: "0px 4px 12px 0px rgba(0, 0, 0, 0.16)", backdropFilter: "blur(28px)" }}
+                        className="bg-white w-[48px] h-[48px] rounded-[999px] p-2 flex items-center justify-center "
                         onClick={handleCommentBtn}
                     >
                         <Image src="blackComment.svg" alt="아이콘" width={26} height={26} />
@@ -120,8 +119,7 @@ const Modal = ({ isVisible, onClose, children }: any) => {
                 {/* 공유하기 */}
                 <div className="flex flex-col items-center justify-center gap-2">
                     <button
-                        className="bg-[rgba(255,255,255,0.80)] w-[48px] h-[48px] rounded-[999px] p-2 flex flex-row gap-2 items-center justify-center ease-in-out duration-300 hover:bg-nonegray hover:bg-opacity-20"
-                        style={{ boxShadow: "0px 4px 12px 0px rgba(0, 0, 0, 0.16)", backdropFilter: "blur(28px)" }}
+                        className="bg-white w-[48px] h-[48px] rounded-[999px] p-2 flex items-center justify-center"
                         onClick={handleShareBtn}
                     >
                         <Image src="share.svg" alt="아이콘" width={25} height={25} />
