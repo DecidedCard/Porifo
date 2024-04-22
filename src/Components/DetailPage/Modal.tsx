b1c9c49eb00ff1ca11deb6fb4d36c0f7463325c5
CodHan <qpscm662@naver.com>
2024-04-22 20:40:38 +0900

    [Style] - 모달창 좋아요,댓글,공유하기 버튼 UI 작업

--- src/Components/DetailPage/Modal.tsx
+++ src/Components/DetailPage/Modal.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Image from "next/image";
+
 const Modal = ({ isVisible, onClose, children }: any) => {
     if (!isVisible) return null;
     const handleClose = (e: any) => {
@@ -14,12 +16,47 @@ const Modal = ({ isVisible, onClose, children }: any) => {
             id="wrapper"
             onClick={handleClose}
         >
-            <div className="flex">
-                <button className="text-white text-xl place-self-start mr-3 mt-2" onClick={onClose}>
-                    X
-                </button>
-                <div className="p-2">{children}</div>
+            <button
+                className="bg-[rgba(255,255,255,0.80)] rounded-[999px] w-[48px] h-[48px] p-2 flex flex-row gap-2 items-center justify-center shrink-0 absolute left-[22%] top-[25px] ease-in-out duration-300 hover:bg-nonegray hover:bg-opacity-20"
+                style={{ boxShadow: "0px 4px 12px 0px rgba(0, 0, 0, 0.16)", backdropFilter: "blur(28px)" }}
+                onClick={onClose}
+            >
+                <Image src="icon-set9.svg" alt="아이콘" width={25} height={25} />
+            </button>
+            {/* 오른쪽 */}
+            <div className="absolute right-[22%] top-[25px] flex flex-col gap-[28px] ">
+                <div className="flex flex-col items-center justify-center gap-2">
+                    <button
+                        className="bg-[rgba(255,255,255,0.80)] w-[48px] h-[48px] rounded-[999px] p-2 flex flex-row gap-2 items-center justify-center ease-in-out duration-300 hover:bg-nonegray hover:bg-opacity-20"
+                        style={{ boxShadow: "0px 4px 12px 0px rgba(0, 0, 0, 0.16)", backdropFilter: "blur(28px)" }}
+                        onClick={onClose}
+                    >
+                        <Image className="block hover:hidden" src="grayHeart.svg" alt="아이콘" width={30} height={30} />
+                    </button>
+                    <span className="text-white font-spoqaBold text-[12px]">좋아요</span>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-2">
+                    <button
+                        className="bg-[rgba(255,255,255,0.80)] w-[48px] h-[48px] rounded-[999px] p-2 flex flex-row gap-2 items-center justify-center ease-in-out duration-300 hover:bg-nonegray hover:bg-opacity-20"
+                        style={{ boxShadow: "0px 4px 12px 0px rgba(0, 0, 0, 0.16)", backdropFilter: "blur(28px)" }}
+                        onClick={onClose}
+                    >
+                        <Image src="blackComment.svg" alt="아이콘" width={26} height={26} />
+                    </button>
+                    <span className="text-white font-spoqaBold text-[12px]">댓글</span>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-2">
+                    <button
+                        className="bg-[rgba(255,255,255,0.80)] w-[48px] h-[48px] rounded-[999px] p-2 flex flex-row gap-2 items-center justify-center ease-in-out duration-300 hover:bg-nonegray hover:bg-opacity-20"
+                        style={{ boxShadow: "0px 4px 12px 0px rgba(0, 0, 0, 0.16)", backdropFilter: "blur(28px)" }}
+                        onClick={onClose}
+                    >
+                        <Image src="share.svg" alt="아이콘" width={25} height={25} />
+                    </button>
+                    <span className="text-white font-spoqaBold text-[12px]">공유하기</span>
+                </div>
             </div>
+            <div className="p-2">{children}</div>
         </div>
     );
 };
