afdf7e03b53d3e1353b33b6a4c2f2c33f0f2b082
DecidedCard <heajun35@gmail.com>
2024-04-22 20:28:37 +0900

    [Style] - 태그

--- src/Components/MyPage/Introduction.tsx
+++ src/Components/MyPage/Introduction.tsx
@@ -68,7 +68,7 @@ const Introduction = () => {
 
                 <hr className="w-[657px] mx-auto border border-neutral-100 sm:w-[60%]" />
 
-                <div className="flex gap-3 sm:w-[60%] mx-auto">
+                <div className="flex gap-3 h-56 sm:w-[60%] mx-auto">
                     <label className="flex font-medium text-zinc-500 w-[177px] h-[32px] mt-2">
                         기술 스택<span className="ml-1 text-[10px] text-red-500">★</span>
                     </label>
@@ -79,6 +79,7 @@ const Introduction = () => {
                                 <input
                                     type="search"
                                     className=" w-[425px] h-8 rounded-[999px] text-sm focus:outline-none"
+                                    placeholder="커스텀 태그를 추가 할 수 있습니다."
                                     value={skillTagInput}
                                     onChange={onChangeSkillTagInputHandler}
                                 />
@@ -90,7 +91,7 @@ const Introduction = () => {
                                     return (
                                         <div
                                             key={idx}
-                                            className="flex gap-2 py-[2px] px-3 w-fit h-[18px] text-[10px] font-medium text-white border border-solid border-primary bg-primary rounded"
+                                            className="flex gap-2 py-[2px] px-3 w-fit h-[18px] text-[10px] font-medium border border-solid border-primary rounded"
                                         >
                                             {item}
                                             <p
@@ -103,21 +104,19 @@ const Introduction = () => {
                                     );
                                 })}
                         </div>
-                        <div className="hidden group-hover:block">
-                            <div className="flex flex-wrap justify-between gap-2 w-[460px] h-[48px] overflow-hidden bg-white sm:w-full sm:content-center">
-                                {SKILL_TAG.map((item, idx) => {
-                                    return (
-                                        <div key={idx}>
-                                            <div
-                                                className="py-[2px] px-3 h-[18px] text-[10px] font-medium border border-solid border-nonegray rounded cursor-pointer"
-                                                onClick={() => onClickSkillTagHandler(item)}
-                                            >
-                                                {item}
-                                            </div>
+                        <div className="flex flex-wrap justify-between gap-2 w-[460px] bg-white sm:w-full sm:content-center">
+                            {SKILL_TAG.map((item, idx) => {
+                                return (
+                                    <div key={idx}>
+                                        <div
+                                            className="py-[2px] px-3 h-[18px] text-[10px] font-medium border border-solid border-nonegray rounded cursor-pointer"
+                                            onClick={() => onClickSkillTagHandler(item)}
+                                        >
+                                            {item}
                                         </div>
-                                    );
-                                })}
-                            </div>
+                                    </div>
+                                );
+                            })}
                         </div>
                     </div>
                 </div>
