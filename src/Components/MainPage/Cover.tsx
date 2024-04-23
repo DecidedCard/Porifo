d72975911642bb350202b58f098d4c47a743b166
leejieun2023 <lge9380@naver.com>
2024-04-23 15:26:09 +0900

    [Style:#201] - 커버 컴포넌트 반응형 완성

--- src/Components/MainPage/Cover.tsx
+++ src/Components/MainPage/Cover.tsx
@@ -15,17 +15,17 @@ const Cover = () => {
                 <source src="/assets/video/mainVideo.mp4" type="video/mp4" />
             </video>
             <div className="w-full h-full absolute top-0 left-0 bg-black opacity-50"></div>
-            <div className="flex flex-col items-center justify-center mt-10 absolute top-[170px] w-full sm:-mt-40">
-                <p className="text-white text-center font-bold text-[80px] leading-[100px] font-spoqaBold sm:text-[30px]">
+            <div className="flex flex-col items-center justify-center mt-10 absolute top-[170px] w-full sm:-mt-44">
+                <p className="text-white text-center font-bold text-[80px] leading-[100px] font-spoqaBold sm:text-[22px] sm:-mb-5">
                     For Your Career, PORIFO
                 </p>
-                <p className="text-white text-center font-bold text-[40px] font-spoqaMedium-bold sm:text-[18px]">
+                <p className="text-white text-center font-bold text-[40px] font-spoqaMedium-bold sm:text-[14px] sm:font-normal">
                     포리포에서 당신의 커리어를 넓혀보세요.
                 </p>
                 <div className="pt-[85px] flex flex-col gap-2 items-center justify-center sm:pt-5">
                     <div className="w-[192px]">
                         <Link href={user ? "/mypage" : "/guest"}>
-                            <div className="flex justify-center items-center py-1 px-3 w-[210px] h-[58px] bg-primary rounded-lg text-white text-base font-medium sm:w-44 sm:h-7 sm:text-[12px] sm:mx-auto">
+                            <div className="flex justify-center items-center py-1 px-3 w-[210px] h-[58px] bg-primary rounded-lg text-white text-base font-medium sm:w-[192px] sm:h-12 sm:text-[16px] sm:mx-auto">
                                 바로 포트폴리오 작성하기
                             </div>
                         </Link>
