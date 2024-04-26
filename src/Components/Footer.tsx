8911f33862459f6b04fc7a67a050f18749d701d1
leejieun2023 <lge9380@naver.com>
2024-04-26 13:12:11 +0900

    [Style:#212] - 메인 반응형 수정

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,20 +2,40 @@ import Image from "next/image";
 
 const Footer = () => {
     return (
-        <div className="bg-hihigray -z-10 h-[172px] px-[190px] py-[60px] flex flex-row gap-6 items-start sm:w-full sm:-ml-44 sm:flex-col">
-            <Image
-                className="flex flex-col items-start justify-start w-[104px] h-[34.5px] overflow-visible"
-                src="../porifo.svg"
-                alt="포리포 로고"
-                width={80}
-                height={80}
-            />
-            <div className="w-[1410px] h-[52px] flex flex-col gap-3 items-start justify-start text-sm text-gray3 sm:text-[12px] sm:w-[440px]">
-                <p className="w-[622px] h-[20px] sm:w-[440px]">
-                    (주)포리포 | 대표 : 먼작귀 | 개인정보보호책임자 : 금상호 | 생성일 : 2024년 3월 26일
-                </p>
-                <p className="w-[410px] h-[22px]">경상북도 안동시 육사로 333 | ©️PORIFO. All rights reverved.</p>
+        <div className="bg-hihigray -z-10 h-[172px] px-[190px] py-[60px] flex flex-row gap-6 items-start sm:items-center sm:justify-center sm:w-full sm:h-[318px] sm:py-10 sm:px-9 sm:flex-col">
+            
+            <div className="sm:hidden">
+                <Image
+                    className="flex flex-col items-start justify-start w-[104px] h-[34.5px] overflow-visible"
+                    src="../porifo.svg"
+                    alt="포리포 로고"
+                    width={80}
+                    height={80}
+                />
+                <div className="w-[1410px] h-[52px] flex flex-col gap-3 items-start justify-start text-sm text-gray3 sm:text-[12px] sm:w-full">
+                    <p className="w-[622px] h-[20px] sm:w-full">
+                        (주)포리포 | 대표 : 먼작귀 | 개인정보보호책임자 : 금상호 | 생성일 : 2024년 3월 26일
+                    </p>
+                    <p className="w-[410px] h-[22px] sm:w-full">경상북도 안동시 육사로 333 | ©️PORIFO. All rights reverved.</p>
+                </div>
             </div>
+
+            <div className="hidden sm:flex sm:justify-center sm:items-center sm:flex-col sm:w-full sm:text-gray3 sm:gap-3">
+                <Image
+                    className="flex flex-col items-center justify-center overflow-visible sm:mb-4"
+                    src="../porifo.svg"
+                    alt="포리포 로고"
+                    width={80}
+                    height={80}
+                />
+                <p>(주)포리포</p>
+                <p>대표 : 먼작귀</p>
+                <p>개인정보보호책임자 : 금상호</p>
+                <p>생성일 : 2024년 3월 26일</p>
+                <p>경상북도 안동시 육사로 333</p>
+                <p>©️PORIFO. All rights reverved.</p>
+            </div>
+
         </div>
     );
 };
