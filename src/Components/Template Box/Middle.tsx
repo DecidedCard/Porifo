d0dd23a2b8d81ae7efbc969ce672df95e91ec5c5
leejieun2023 <lge9380@naver.com>
2024-04-26 21:28:51 +0900

    [Style:#212] - 박스 템플릿 반응형 수정

--- src/Components/Template Box/Middle.tsx
+++ src/Components/Template Box/Middle.tsx
@@ -14,8 +14,8 @@ const Middle = ({ portfolio }: { portfolio: PortfolioInfo }) => {
 
             {userSkillTag.length > 0 && (
                 <div className="flex flex-col items-start justify-start mt-10 sm:items-center sm:justify-center">
-                    <p className="font-medium text-[20px] w-[804px] sm:w-[448px] sm:font-medium sm:text-[20px]">기술스택</p>
-                    <div className="flex flex-wrap text-primary text-[12px] sm:w-[448px]">
+                    <p className="font-medium text-[20px] w-[804px] sm:w-[370px] sm:font-medium sm:text-[20px]">기술스택</p>
+                    <div className="flex flex-wrap text-primary text-[12px] sm:w-[370px]">
                         {userSkillTag.map((tag, index) => (
                             <span
                                 key={index}
@@ -25,15 +25,15 @@ const Middle = ({ portfolio }: { portfolio: PortfolioInfo }) => {
                             </span>
                         ))}
                     </div>
-                    <div className="bg-deepgray w-[804px] h-[1px] my-10 sm:w-[448px]"></div>
+                    <div className="bg-deepgray w-[804px] h-[1px] my-10 sm:w-[370px]"></div>
                 </div>
             )}
 
 
             <div className="flex flex-col items-start justify-start sm:items-center sm:justify-center">
-                <p className="font-medium text-[20px] mb-4 sm:w-[448px] sm:font-medium sm:text-[20px]">자기소개</p>
-                <p className="text-[14px] w-[804px] tracking-wide leading-normal sm:w-[448px] sm:text-gray4">{userInfo.introduce}</p>
-                <div className="bg-deepgray w-[804px] h-[1px] my-10 sm:w-[448px]"></div>
+                <p className="font-medium text-[20px] mb-4 sm:w-[370px] sm:font-medium sm:text-[20px]">자기소개</p>
+                <p className="text-[14px] w-[804px] tracking-wide leading-normal sm:w-[370px] sm:text-gray4">{userInfo.introduce}</p>
+                <div className="bg-deepgray w-[804px] h-[1px] my-10 sm:w-[370px]"></div>
             </div>
         </div>
     );
