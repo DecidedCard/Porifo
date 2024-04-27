5e5b1156e08c5afe6e9e849c3749ccc5b970fbf0
DecidedCard <heajun35@gmail.com>
2024-04-27 19:47:22 +0900

    [Feat:#159] - build error 해결

--- src/app/(authlayout)/layout.tsx
+++ src/app/(authlayout)/layout.tsx
@@ -9,23 +9,7 @@ import LoginCheckModal from "@/Components/LoginCheckModal";
 import useLoginCheck from "@/hooks/mypage/useLoginCheck";
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
-    const { isFetching, isError } = useLoginCheck();
-
-    if (isFetching) {
-        return (
-            <div className="absolute top-0 left-0 z-50 flex justify-center items-center w-screen h-screen bg-hihigray">
-                <Loading />
-            </div>
-        );
-    }
-
-    if (isError) {
-        return (
-            <div>
-                <LoginCheckModal route="/" />
-            </div>
-        );
-    }
+    useLoginCheck();
 
     return (
         <div>
