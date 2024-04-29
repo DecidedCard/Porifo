cb03ad59495fabed81841d272ecb7fd343d293c8
DecidedCard <heajun35@gmail.com>
2024-04-29 20:31:41 +0900

    [Feat] - 토스트 작성

--- src/hooks/mypage/useLoginCheck.ts
+++ src/hooks/mypage/useLoginCheck.ts
@@ -2,10 +2,6 @@ import { useEffect } from "react";
 
 import useUserStore from "@/store/userStore";
 
-import { QUERY_KEY } from "@/util/query_key";
-import { userData } from "@/util/supabase/supabase_user";
-
-import { useQuery } from "@tanstack/react-query";
 import useUserCheck from "../useUserCheck";
 
 const useLoginCheck = () => {
