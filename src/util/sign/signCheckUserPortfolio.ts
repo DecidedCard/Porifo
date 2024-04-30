4c5d2cd5ea48a5137c7c1ac7462d78b6c9abff2b
sangho <keumm9635@gmail.com>
2024-04-30 12:29:13 +0900

    사용되지 않는 코드 삭제

--- src/util/sign/signCheckUserPortfolio.ts
+++ src/util/sign/signCheckUserPortfolio.ts
@@ -12,13 +12,6 @@ const signCheckUserPortfolio = async ({ setRedirecTo }: RedirectType) => {
         setRedirecTo(redirectTo);
         return;
     }
-
-    // user?.user_metadata.birthDate !== undefined && user?.user_metadata.sex !== undefined
-    //     ? havePortfolio
-    //         ? (redirectTo = `${process.env.NEXT_PUBLIC_BASE_URL}/community`)
-    //         : (redirectTo = `${process.env.NEXT_PUBLIC_BASE_URL}/mypage`)
-    //     : (redirectTo = `${process.env.NEXT_PUBLIC_BASE_URL}/socialSetting`);
-    // setRedirecTo(redirectTo);
 };
 
 export default signCheckUserPortfolio;
