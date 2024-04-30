79309acb013610308a4a4921eb233401b175c14e
DecidedCard <heajun35@gmail.com>
2024-04-30 20:37:13 +0900

    [Style] - 반응형 작성 중

--- src/Components/MyPage/Introduction.tsx
+++ src/Components/MyPage/Introduction.tsx
@@ -94,7 +94,7 @@ const Introduction = () => {
                                 </form>
                             </div>
 
-                            <div className="flex flex-wrap gap-2 w-[460px] h-11 overflow-hidden bg-white sm:content-center sm:justify-start sm:w-full">
+                            <div className="flex flex-wrap gap-2 w-[460px] h-11 overflow-hidden bg-white sm:justify-start sm:w-full">
                                 {skill_tag.map((item, idx) => {
                                     return (
                                         <div key={idx}>
