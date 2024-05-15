cb7e34d4ff9a6e485eba13d46230d3e1f2cfb8b2
heajun jeong <heajun35@gmail.com>
2024-05-15 13:29:43 +0900

    [Feat:314] - 학력 커스텀 훅 추가

---
+++ src/hooks/mypage/useEducation.ts
@@ -0,0 +1,55 @@
+import { ChangeEvent } from "react";
+
+import useEducationStore from "@/store/educationStore";
+const useCareer = () => {
+    const { education, setSchool, setClass, setDate, setComment, setAddEducation, setMinusEducation } =
+        useEducationStore();
+
+    const onChangeSchoolHandler = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+        setSchool(e.target.value, index);
+    };
+
+    const onChangeClassHandler = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+        setClass(e.target.value, index);
+    };
+
+    const onChangeDateHandler = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+        const { name, value } = e.target;
+
+        if (name === "startDate") {
+            const startDate = name === "startDate" && value;
+            setDate(`${startDate} ~ ${education[index].date.slice(10)}`, index);
+            return;
+        }
+
+        if (name === "endDate") {
+            const endDate = name === "endDate" && value;
+            setDate(`${education[index].date.slice(0, 7)} ~ ${endDate}`, index);
+            return;
+        }
+    };
+
+    const onChangeCommentHandler = (e: ChangeEvent<HTMLTextAreaElement>, index: number) => {
+        setComment(e.target.value, index);
+    };
+
+    const onClickAddHandler = () => {
+        setAddEducation();
+    };
+
+    const onClickMinusHandler = (arg: number) => {
+        setMinusEducation(arg);
+    };
+
+    return {
+        education,
+        onChangeSchoolHandler,
+        onChangeClassHandler,
+        onChangeDateHandler,
+        onChangeCommentHandler,
+        onClickAddHandler,
+        onClickMinusHandler,
+    };
+};
+
+export default useCareer;
