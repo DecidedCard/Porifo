3e2a299d8d096b380acbd9add93d6930cd48b714
heajun jeong <heajun35@gmail.com>
2024-09-06 21:29:33 +0900

    [Refactor:329] - 이미지 슬라이드 코드 리펙토링

--- src/Components/MainPage/MiddleImage.tsx
+++ src/Components/MainPage/MiddleImage.tsx
@@ -1,107 +1,29 @@
 import Image from "next/image";
 
+const image = [
+    "/assets/image/slide1.svg",
+    "/assets/image/slide2.svg",
+    "/assets/image/slide3.svg",
+    "/assets/image/slide4.svg",
+    "/assets/image/slide5.svg",
+    "/assets/image/slide6.svg",
+    "/assets/image/slide7.svg",
+];
+
 const MiddleImage = () => {
     return (
         <main className="overflow-hidden w-full sm:w-full">
             <div className="flex flex-row gap-8 items-start justify-start animate-slider">
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide1.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide2.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide3.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide4.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide5.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide6.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide7.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide1.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide2.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide3.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide4.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide5.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide6.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
-                <Image
-                    className="rounded-2xl sm:w-[250px] sm:h-[274px]"
-                    src="/assets/image/slide7.svg"
-                    alt="슬라이드 이미지"
-                    width={370}
-                    height={400}
-                />
+                {image.map((item, idx) => (
+                    <Image
+                        key={idx}
+                        className="rounded-2xl sm:w-[250px] sm:h-[274px]"
+                        src={item}
+                        alt="슬라이드 이미지"
+                        width={370}
+                        height={400}
+                    />
+                ))}
             </div>
         </main>
     );
