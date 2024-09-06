1d05e1a160b8d72e50209b4f8a04fab7e88990a4
heajun jeong <heajun35@gmail.com>
2024-09-06 20:10:55 +0900

    [Feat:329] - 사용하지 않는 임포트 정리

--- src/app/(authlayout)/layout.tsx
+++ src/app/(authlayout)/layout.tsx
@@ -2,8 +2,6 @@
 
 import React, { PropsWithChildren } from "react";
 import Header from "@/Components/Header";
-import Footer from "@/Components/Footer";
-import Loading from "@/Components/Loading";
 import LoginCheckModal from "@/Components/LoginCheckModal";
 
 import useLoginCheck from "@/hooks/mypage/useLoginCheck";
