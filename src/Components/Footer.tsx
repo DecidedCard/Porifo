bb942053491e0fff8dac106a230f1d04de90a795
heajun <heajun35@gmail.com>
2025-12-02 22:37:29 +0900

    Fix: 커뮤니티 페이지 css 수정

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,8 +2,7 @@ import Image from "next/image";
 
 const Footer = () => {
     return (
-        <div className="bg-hihigray -z-10 h-[172px] px-[190px] py-[60px] flex flex-row gap-6 items-start sm:items-center sm:justify-center sm:w-full sm:h-[318px] sm:py-10 sm:px-9 sm:flex-col">
-            
+        <div className="bg-gray_1 -z-10 h-[172px] px-[190px] py-[60px] flex flex-row gap-6 items-start sm:items-center sm:justify-center sm:w-full sm:h-[318px] sm:py-10 sm:px-9 sm:flex-col">
             <div className="sm:hidden">
                 <Image
                     className="flex flex-col items-start justify-start w-[104px] h-[34.5px] overflow-visible mb-5"
@@ -12,7 +11,7 @@ const Footer = () => {
                     width={80}
                     height={80}
                 />
-                <div className="w-[1410px] h-[52px] flex flex-col gap-1 items-start justify-start text-sm text-gray3">
+                <div className="w-[1410px] h-[52px] flex flex-col gap-1 items-start justify-start text-P7_R text-gray_4">
                     <p className="w-[622px] h-[20px] sm:w-full">
                         (주)포리포 | 대표 : 먼작귀 | 개인정보보호책임자 : 금상호 | 생성일 : 2024년 3월 26일
                     </p>
@@ -20,7 +19,7 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="hidden sm:flex sm:justify-center sm:items-center sm:flex-col sm:w-full sm:text-gray3 sm:gap-3 sm:text-[12px]">
+            <div className="hidden sm:flex sm:justify-center sm:items-center sm:flex-col sm:w-full sm:text-gray_4 sm:gap-3 sm:text-P8_R">
                 <Image
                     className="flex flex-col items-center justify-center overflow-visible sm:mb-4"
                     src="../porifo.svg"
@@ -34,7 +33,6 @@ const Footer = () => {
                 <p>생성일 : 2024년 3월 26일</p>
                 <p>©️PORIFO. All rights reverved.</p>
             </div>
-
         </div>
     );
 };
--- src/app/community/layout.tsx
+++ src/app/community/layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 import useLoginCheck from "@/hooks/mypage/useLoginCheck";
 
-export default function FindPasswordLayout({
+export default function CommunityLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
